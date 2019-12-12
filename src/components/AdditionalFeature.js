1c166f754cfc89bf288d5bc7f5888b1d9e06e194
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2019-12-12 09:09:14 -0600

    mvp

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { addFeature } from '../actions/carActions'
+import { connect } from 'react-redux';
 
 const AdditionalFeature = props => {
+
+const buyItem = item => {
+  props.addFeature(item)
+  // dispatch an action here to add an item
+}
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={() => buyItem(props.feature) }>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
+//see explaination in added feature
+const mapDispatchToProps = {
+  addFeature
+}
 
-export default AdditionalFeature;
+export default connect(state => state, mapDispatchToProps )(AdditionalFeature);
---
+++ src/components/reducers/CarReducer.js
@@ -0,0 +1,57 @@
+
+
+
+
+
+export const initialState = {
+    additionalPrice: 0,
+        car: {
+            price: 26395,
+            name: '2019 Ford Mustang',
+            image:
+                'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+            features: []
+      },
+        additionalFeatures: [
+            { id: 1, name: 'V-6 engine', price: 1500 },
+            { id: 2, name: 'Racing detail package', price: 1500 },
+            { id: 3, name: 'Premium sound system', price: 500 },
+            { id: 4, name: 'Rear spoiler', price: 250 }
+        ]
+    };
+
+export const carReducer = (state = initialState, action) => {
+    switch (action.type) {
+    //add feature that will add amount to current price
+        case 'ADD_FEATURE':
+            const addOn = 
+//filter, map didn't work--this returns the value of the first element that satisifies true statement
+                state.additionalFeatures.find(item => item.id === action.payload.id);
+            return {
+                ...state, 
+                additionalPrice: 
+                    (state.additionalPrice + action.payload.price),
+                car: {
+                    ...state.car,
+                features: 
+                    [...state.car.features, addOn]
+                }
+            }
+    //remove feature that will subtract price from current price
+        case 'REMOVE_FEATURE':
+            return {
+                ...state,
+                additionalPrice:
+                    (state.additionalPrice - action.payload.price),
+                car: {
+                    ...state.car,
+                features: 
+                    state.car.features.filter(item => item.id !== action.payload.id)    
+                }
+
+            }
+        default:
+            return state;
+    }
+    
+}
